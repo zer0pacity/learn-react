afa81c252b5b8068180b934ff167d2a1999e7dbb
zer0pacity <62236125+zer0pacity@users.noreply.github.com>
2022-07-31 20:23:24 +0900

    to 2.2

---
+++ styling-with-css-module/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import CheckBox from './components/Checkbox';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  };
+
+  return (
+    <div>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? 'true' : 'false'}
+      </p>
+    </div>
+  );
+}
+
+export default App;
